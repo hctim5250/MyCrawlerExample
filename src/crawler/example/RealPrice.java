871609ffe5807391664b16d49ba2766308ac1a17
hctin5250 <hctim5250@gmail.com>
2017-12-09 14:46:40 +0800

    20171209

--- src/crawler/example/RealPrice.java
+++ src/crawler/example/RealPrice.java
@@ -20,7 +20,7 @@ public class RealPrice {
 		
 		String uri = "zip:http://plvr.land.moi.gov.tw"
 				+ "/Download?type=zip&fileName=lvr_landxml.zip"
-				+ "!/E_LVR_LAND_A.XML";
+				+ "!/B_LVR_LAND_A.XML";
 
 
 		Document jsoupDoc = CrawlerPack.start()
